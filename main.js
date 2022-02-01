fc0ec26bb85b1a418828804c8d58492c279ef81c
Marina De Gaetani <marina.degaetani03@gmail.com>
2022-02-01 07:57:04 +0100

    changement des images + ajout css + js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 //Ajout des constantes :
-
+const CLEAPI = "4c398198a97ce519dfc543aae27c25f4";
+let resultatsAPI;
+const temps = document.querySelector(".temps");
+const temperature = document.querySelector(".temperature");
+const localisation = document.querySelector(".localisation");
 const dateHeure = document.querySelector("date_heure");
 const hours = document.querySelectorAll("hours");
 const days = document.querySelectorAll("days");
@@ -16,7 +20,7 @@ const getWeatherOf = async (position) => {
     const lon = position.coords.longitude;
 
     const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&lang=fr&appid=4c398198a97ce519dfc543aae27c25f4`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&lang=fr&appid=${CLEAPI}`
     );
     const data = await res.json();
     updateUI(data);
@@ -29,6 +33,7 @@ const updateUI = (data) => {
 };
 //autre manière de faire :
 // function getWeatherOf = (lat, lon) => {}
+
 //Demande de geocalisation
 navigator.geolocation.getCurrentPosition(
   getWeatherOf,
@@ -37,5 +42,4 @@ navigator.geolocation.getCurrentPosition(
 );
 
 //TODO ajout des villes avec une API
-//TODO ajout heure et date
 //TODO changement icon et image en fonction du temps et du moment de la journée
